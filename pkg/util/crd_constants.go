cc81746f542dc6532cafb0f4bcdaa5f0e34d6414
msenmurugan <msenmurugan@gmail.com>
2020-04-24 12:29:43 -0400

    * delete unused code * renamed the crd_defaults to crd_constants

---
+++ pkg/util/crd_constants.go
@@ -0,0 +1,44 @@
+/*
+Copyright (C) 2019 Synopsys, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements. See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership. The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package util
+
+const (
+	// AlertCRDName is the name of an Alert CRD
+	AlertCRDName = "alerts.synopsys.com"
+	// BlackDuckCRDName is the name of the Black Duck CRD
+	BlackDuckCRDName = "blackducks.synopsys.com"
+	// OpsSightCRDName is the name of an OpsSight CRD
+	OpsSightCRDName = "opssights.synopsys.com"
+	// PrmCRDName is the name of the Polaris Reporting Module CRD
+	PrmCRDName = "prms.synopsys.com"
+
+	// OperatorName is the name of an Operator
+	OperatorName = "synopsys-operator"
+	// AlertName is the name of an Alert app
+	AlertName = "alert"
+	// BlackDuckName is the name of the Black Duck app
+	BlackDuckName = "blackduck"
+	// OpsSightName is the name of an OpsSight app
+	OpsSightName = "opssight"
+	// PrmName is the name of the Prm app
+	PrmName = "prm"
+)
